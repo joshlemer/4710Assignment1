e474b9ee3324c7f91c6448c616b25ff558206b11
Josh Lemer <joshlemer@gmail.com>
2014-10-12 14:15:35 -0500

    kmeans is working

--- myKMeans.py
+++ myKMeans.py
@@ -18,9 +18,7 @@ def load_data_from_file(f):
 thedata = load_data_from_file("phonedata.txt")
 thelabels = load_data_from_file("activitylabels.txt")
 thelabelsmap = ['a','b','c','d','e','f']
-thelabelsstrings = []
-for i in thelabels:
-    thelabelsstrings.append(thelabelsmap[int(i) - 1])
+thelabelsstrings = np.matrix([['a'] for x in thelabels])
 
 thelabelsstrings = np.matrix(thelabelsstrings)
 thelabelsmap = np.matrix(thelabelsmap)
@@ -31,8 +29,9 @@ Note that for PCA you may use built-in functions, you should not though for your
 """
 
 B = mplml.PCA(thedata)
-plt.scatter(B.Y[:,0],B.Y[:,1], c=np.array(thelabels.transpose()), label=['asdf','hello'])
-plt.legend(numpoints=1)
+index = 0
+plt.scatter(B.Y[:,0],B.Y[:,1], c=np.array(thelabels.transpose()))#, label=np.array(thelabelsstrings.transpose()))
+#plt.legend()
 plt.title('PCA projection of phonedata in two dimensions')
 plt.show()
 
@@ -45,39 +44,66 @@ Now implement K-Means
 There are built in functions for this - but the should be done by you!
 """
 
+#recalculates the center of each cluster in kclusters
+def update_centroids(kclusters, centroids):
+    index = 0
+    for cluster in kclusters:
+        if len(cluster) > 0:
+            centroids[index] = np.mean(cluster, axis=0)
+        index += 1
+
+    return centroids
+
+def SSE(centroids, kclusters):
+    cluster_sse = [0 for x in range(len(kclusters))]
+    sse = 0
+    index = 0
+    for centroid in centroids:
+        for point in kclusters[index]:
+            squared_error = np.square(np.linalg.norm(centroid - point))
+            sse += squared_error
+            cluster_sse[index] += squared_error
+        index+= 1
+
+    return sse, cluster_sse
+def perform_kmeans(kvalue, numiters, thedata ):
+    centroids = np.zeros((kvalue,561))
+
+    #Assigns each point to a cluster
+    cluster_assignments = [random.randint(1,kvalue) for _ in range(rows)]
+
+    for iteration in range(numiters):
+
+        #Partition clusters into a list of lists of points
+        kclusters = [[] for x in range(kvalue)]
+        index = 0
+        for assignment in cluster_assignments:
+            kclusters[assignment - 1].append(thedata[index])
+            index += 1
+
+        centroids = update_centroids(kclusters, centroids)
+
+        index = 0
+        for point in thedata:
+            centroid_distances = [np.linalg.norm(point - x) for x in centroids]
+            cluster_assignments[index] = np.argmin(centroid_distances) + 1
+            index += 1
+
+    sse, cluster_sse = SSE(centroids, kclusters)
+    return cluster_assignments, sse, cluster_sse
 # May wish to initialize k-value, iterations and centroids
 kvalue = 6
 numiters = 10
 rows, columns = thedata.shape
-centroid = np.zeros((kvalue,561))
-cluster_assignments = [random.randint(1,kvalue) for _ in range(rows)]
-#print cluster_assignments
+centroids = np.zeros((kvalue,561))
 
-centroids = [0 for x in range(kvalue)]
-for iteration in range(numiters):
-    a = 5
-    #calculate new centroids
-    kclusters = [[] for x in range(kvalue)]
-    index = 0
-    for assignment in cluster_assignments:
-        kclusters [assignment - 1].append(thedata[index])
-        index += 1
-
-    ##calculate mean of all points assigned to clusters
-    index = 0
-    for cluster in kclusters:
-        if(len(cluster) > 0):
-            centroids[index]= np.mean(cluster, axis=0)
-
-        index += 1
+cluster_assignments, sse, cluster_sse = perform_kmeans(kvalue,numiters,thedata)
 
 
-    index = 0
-    for point in thedata:
-        centroid_distances = [np.linalg.norm(point - x) for x in centroids]
-        cluster_assignments[index] = np.argmin(centroid_distances)
-        index += 1
-
+plt.scatter(B.Y[:,0],B.Y[:,1], c=np.array(np.matrix(cluster_assignments).transpose()))#, c=np.array(thelabels.transpose()), label=np.array(thelabelsstrings.transpose()))
+#plt.legend()
+plt.title('PCA projection of phonedata in two dimensions')
+plt.show()
 
     ##reassign points to closest centroid
     #for point in thedata
